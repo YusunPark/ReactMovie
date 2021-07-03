727d319e45f82ee9f34cf27f9f2e2f36af5eee7d
Yusun <solarpy@hanyang.ac.kr>
2021-07-03 17:14:42 +0900

    Add Movie.js for get movie_info

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from "react";
 import axios from "axios";
-// import PropTypes from "prop-types";
-
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
     isLoading: true,
-    movie: []
+    movies: []
   };
   getMovies = async () => {
-    const movies = awaite axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json")
+    const {data: {data : {movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating")
+    this.setState({movies:movies, isLoading: false})
   };
   
   componentDidMount() {
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
-      <div>{isLoading ? "Loading" : "We are ready"}</div> 
+      <div>{isLoading ? "Loading.." : movies.map( movie => {
+        return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}/>
+      })}</div> 
     );
   }
 }
