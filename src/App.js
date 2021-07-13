1040ad2c71cd8f4210c28c6810eba4064ae6ee97
Yusun <solarpy@hanyang.ac.kr>
2021-07-14 06:25:14 +0900

    Add movie genre, fit jdx

--- src/App.js
+++ src/App.js
@@ -27,22 +27,25 @@ class App extends React.Component {
     const { isLoading, movies } = this.state;
     console.log(movies);
     return (
-      <section class="container">
+      <section className="container">
         {isLoading ? (
-          <div class="loader">
-            <span class="loader__text">Loading...</span>
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
           </div>
         ) : (
-          <div class="movies">
+          <div className="movies">
             {movies.map((movie) => {
-             return (<Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-              ></Movie>);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                ></Movie>
+              );
             })}
           </div>
         )}
