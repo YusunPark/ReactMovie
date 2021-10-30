6d6b6834245359338ed36cd95754df099ee66cf0
Yusun <solarpy@hanyang.ac.kr>
2021-10-30 14:06:15 +0900

    Complete chaper 12

--- src/Movie.js
+++ src/Movie.js
@@ -7,16 +7,16 @@ function Movie({ year, title, summary, poster, genres }) {
     <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie__data">
-        <h3 className="movie__title" style={{ backgroundColor: "red" }}>
+        <h3 className="movie__title">
           {title}
         </h3>
         <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
+        <ul className="movie__genres">
           {genres.map((genre, index) => (
-            <li key={index} className="genres__genre">{genre}</li>
+            <li key={index} className="genres__genre">{genre}</li> 
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0,150)}</p>
       </div>
     </div>
   );
